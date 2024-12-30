0cd75a642e343709822a8de8df46d96176dbf26e
Andrés Contreras Guillén <andres.contreras@dworkers.ai>
2024-12-30 20:04:36 +0100

    Update import paths in ProductRepository.java
    Modified import statements to reference the updated package structure under 'core.v1' instead of 'product.v1'. This ensures consistency with the new package organization and resolves potential reference issues.

--- common-platform-product-mgmt-models/src/main/java/com/catalis/core/product/models/repositories/product/v1/ProductRepository.java
+++ common-platform-product-mgmt-models/src/main/java/com/catalis/core/product/models/repositories/product/v1/ProductRepository.java
@@ -1,8 +1,8 @@
 package com.catalis.core.product.models.repositories.product.v1;
 
-import com.catalis.core.product.interfaces.enums.product.v1.ProductStatusEnum;
-import com.catalis.core.product.interfaces.enums.product.v1.ProductTypeEnum;
-import com.catalis.core.product.models.entities.product.v1.Product;
+import com.catalis.core.product.interfaces.enums.core.v1.ProductStatusEnum;
+import com.catalis.core.product.interfaces.enums.core.v1.ProductTypeEnum;
+import com.catalis.core.product.models.entities.core.v1.Product;
 import com.catalis.core.product.models.repositories.BaseRepository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
