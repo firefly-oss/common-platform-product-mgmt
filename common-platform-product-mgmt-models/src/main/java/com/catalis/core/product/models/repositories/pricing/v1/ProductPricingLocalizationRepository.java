c76930131e32cf3a841c970db6b9dbc2e4991337
Andrés Contreras Guillén <andres.contreras@dworkers.ai>
2024-12-31 19:15:47 +0100

    Update findByProductPricingId to support pagination
    Modified the ProductPricingLocalizationRepository to include a Pageable parameter in the findByProductPricingId method. This change enables pagination support, improving performance and scalability for large datasets.

--- common-platform-product-mgmt-models/src/main/java/com/catalis/core/product/models/repositories/pricing/v1/ProductPricingLocalizationRepository.java
+++ common-platform-product-mgmt-models/src/main/java/com/catalis/core/product/models/repositories/pricing/v1/ProductPricingLocalizationRepository.java
@@ -2,13 +2,14 @@ package com.catalis.core.product.models.repositories.pricing.v1;
 
 import com.catalis.core.product.models.entities.pricing.v1.ProductPricingLocalization;
 import com.catalis.core.product.models.repositories.BaseRepository;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
 public interface ProductPricingLocalizationRepository extends BaseRepository<ProductPricingLocalization, Long> {
-    Flux<ProductPricingLocalization> findByProductPricingId(Long pricingId);
+    Flux<ProductPricingLocalization> findByProductPricingId(Long pricingId, Pageable pageable);
 
     Flux<ProductPricingLocalization> findByCurrencyCode(String currencyCode);
 
